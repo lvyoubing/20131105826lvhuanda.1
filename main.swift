97d26470db14958bc479e215dfb15e6b58978f98
lvyoubing <lvhuanda@admindeiMac-88.local>
2016-03-10 10:23:28 +0800

    swift for 循环

---
+++ main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  20131105826lvhuanda.1
+//
+//  Created by 吕欢达 on 16/3/10.
+//  Copyright © 2016年 吕欢达. All rights reserved.
+//
+
+import Foundation
+
+for var i in 1...10 {
+print("\(i)")
+}
